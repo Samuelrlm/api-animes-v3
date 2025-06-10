aad4fd5def16a8ab230ebd10104e0e5e3fd01f42
Samuel Lima <105258182+Samuelrlm@users.noreply.github.com>
2025-06-10 20:42:03 -0300

    add

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const animesRoutes = require('./src/routes/animes');
 const usersRoutes = require('./src/routes/users');
 const authRoutes = require('./src/routes/auth');
@@ -7,6 +8,7 @@ require('./src/models');
 const app = express();
 const port = 4888;
 
+app.use(cors())
 app.use(express.json());
 
 app.use(animesRoutes);
